945a75a951bbbb5bbf26742f90272bf4ad6fb75b
Jeff Borghoff <55985071+jeff-borghoff@users.noreply.github.com>
2022-08-18 18:03:54 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    esp8266.sendTelegramMessage("5708921032:AAHHC_NzXoT6WwvpR1BX2TFUXjSq2yTWJl8", "-739648072", "Watchdog Alert")
+})
 esp8266.init(SerialPin.P16, SerialPin.P15, BaudRate.BaudRate115200)
 if (esp8266.isESP8266Initialized()) {
     basic.showIcon(IconNames.Happy)
 } else {
     basic.showIcon(IconNames.Sad)
 }
-esp8266.connectWiFi("", "")
+esp8266.connectWiFi("summer_winds", "6093066149")
 basic.showString("Connecting")
 if (esp8266.isWifiConnected()) {
     basic.showIcon(IconNames.Happy)
 } else {
     basic.showIcon(IconNames.Sad)
 }
-basic.forever(function () {
-	
-})

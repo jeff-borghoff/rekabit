6647792823ad121d4884991b26e831f17684af0a
Jeff Borghoff <55985071+jeff-borghoff@users.noreply.github.com>
2022-09-03 16:35:35 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     esp8266.sendTelegramMessage("5708921032:AAHHC_NzXoT6WwvpR1BX2TFUXjSq2yTWJl8", "-739648072", "Watchdog Alert")
+    esp8266.sendTelegramMessage("5687772713:AAH7ZAocx78-aLjlnqN2gAgEsX6mu_st-_Q", "-739648072", "Person Alert")
 })
 esp8266.init(SerialPin.P16, SerialPin.P15, BaudRate.BaudRate115200)
 if (esp8266.isESP8266Initialized()) {
@@ -17,8 +18,7 @@ if (esp8266.isWifiConnected()) {
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_RECOGNITION)
 basic.forever(function () {
+    basic.pause(1000)
     huskylens.request()
-    if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        esp8266.sendTelegramMessage("5687772713:AAH7ZAocx78-aLjlnqN2gAgEsX6mu_st-_Q", "-739648072", "Person Alert")
-    }
+    basic.showNumber(huskylens.getIds())
 })

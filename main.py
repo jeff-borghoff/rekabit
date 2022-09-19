c53560af4d0000b4268adfd42983059580a86f0f
Jeff Borghoff <55985071+jeff-borghoff@users.noreply.github.com>
2022-09-19 14:40:19 -0400

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-def on_button_pressed_a():
-    pass
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-DFRobotWiFiIoTUART.wifi_setup(SerialPin.P8, SerialPin.P12, "summer_winds", "6093066149")
-basic.pause(5000)
-if True:
-    pass
+esp8266.init(SerialPin.USB_TX, SerialPin.USB_RX, BaudRate.BAUD_RATE115200)
+if esp8266.is_esp8266_initialized():
+    basic.show_icon(IconNames.YES)
+else:
+    basic.show_icon(IconNames.NO)
+esp8266.connect_wi_fi("", "")
+if esp8266.is_wifi_connected():
+    basic.show_icon(IconNames.YES)
+else:
+    basic.show_icon(IconNames.NO)
 PERSON = 1
 DOG = 2
-basic.show_string(DFRobotWiFiIoTUART.http_get("https://www.google.com", 10000))
 huskylens.init_i2c()
 huskylens.init_mode(protocolAlgorithm.ALGORITHM_OBJECT_RECOGNITION)
+music.set_built_in_speaker_enabled(True)
 
 def on_forever():
+    basic.clear_screen()
     basic.pause(1000)
     huskylens.request()
     if huskylens.is_appear(PERSON, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK):
+        esp8266.send_telegram_message("AAHHC_NzXoT6WwvpR1BX2TFUXjSq2yTWJl8", "", "")
+        if esp8266.is_telegram_message_sent():
+            basic.show_icon(IconNames.YES)
         basic.show_icon(IconNames.STICK_FIGURE)
+        music.play_sound_effect(music.builtin_sound_effect(soundExpression.giggle),
+            SoundExpressionPlayMode.UNTIL_DONE)
     elif huskylens.is_appear(DOG, HUSKYLENSResultType_t.HUSKYLENS_RESULT_BLOCK):
+        esp8266.send_telegram_message("", "", "")
+        if esp8266.is_telegram_message_sent():
+            basic.show_icon(IconNames.YES)
         basic.show_icon(IconNames.GIRAFFE)
+        music.play_sound_effect(music.builtin_sound_effect(soundExpression.twinkle),
+            SoundExpressionPlayMode.UNTIL_DONE)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,23 +1,37 @@
-input.onButtonPressed(Button.A, function () {
-	
-})
-DFRobotWiFiIoTUART.WIFISetup(
-SerialPin.P8,
-SerialPin.P12,
-"summer_winds",
-"6093066149"
-)
-DFRobotWiFiIoTUART.IFTTTConfigure("yourEvent", "yourKey")
+esp8266.init(SerialPin.USB_TX, SerialPin.USB_RX, BaudRate.BaudRate115200)
+if (esp8266.isESP8266Initialized()) {
+    basic.showIcon(IconNames.Yes)
+} else {
+    basic.showIcon(IconNames.No)
+}
+esp8266.connectWiFi("", "")
+if (esp8266.isWifiConnected()) {
+    basic.showIcon(IconNames.Yes)
+} else {
+    basic.showIcon(IconNames.No)
+}
 let PERSON = 1
 let DOG = 2
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_RECOGNITION)
+music.setBuiltInSpeakerEnabled(true)
 basic.forever(function () {
+    basic.clearScreen()
     basic.pause(1000)
     huskylens.request()
     if (huskylens.isAppear(PERSON, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        esp8266.sendTelegramMessage("5708921032:AAHHC_NzXoT6WwvpR1BX2TFUXjSq2yTWJl8", "-739648072", "Dog")
+        if (esp8266.isTelegramMessageSent()) {
+            basic.showIcon(IconNames.Yes)
+        }
         basic.showIcon(IconNames.StickFigure)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
     } else if (huskylens.isAppear(DOG, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        esp8266.sendTelegramMessage("", "", "")
+        if (esp8266.isTelegramMessageSent()) {
+            basic.showIcon(IconNames.Yes)
+        }
         basic.showIcon(IconNames.Giraffe)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.twinkle), SoundExpressionPlayMode.UntilDone)
     }
 })
